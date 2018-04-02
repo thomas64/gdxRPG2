53491f910a1e9b8500bc7e6fd83ba7e48c42207f
Thomas M. Hesselink <mail@t64.nl>
2018-04-02 23:35:44 +0200

    lib-gdx setup

---
+++ ios-moe/src/nl/t64/game/rpg/IOSMoeLauncher.java
@@ -0,0 +1,24 @@
+package nl.t64.game.rpg;
+
+import apple.uikit.c.UIKit;
+import com.badlogic.gdx.backends.iosmoe.IOSApplication;
+import com.badlogic.gdx.backends.iosmoe.IOSApplicationConfiguration;
+import org.moe.natj.general.Pointer;
+
+public class IOSMoeLauncher extends IOSApplication.Delegate {
+
+    protected IOSMoeLauncher(Pointer peer) {
+        super(peer);
+    }
+
+    public static void main(String[] argv) {
+        UIKit.UIApplicationMain(0, null, null, IOSMoeLauncher.class.getName());
+    }
+
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        config.useAccelerometer = false;
+        return new IOSApplication(new Game(), config);
+    }
+}
