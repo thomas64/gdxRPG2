014ad0baf2784cac0d91b6d51d3f741f3e9317a4
Thomas M. Hesselink <mail@t64.nl>
2021-08-22 02:07:34 +0200

    huge speed improvement at load profiles screen.

--- core/src/nl/t64/game/rpg/ProfileManager.kt
+++ core/src/nl/t64/game/rpg/ProfileManager.kt
@@ -3,7 +3,6 @@ package nl.t64.game.rpg
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Preferences
 import com.badlogic.gdx.utils.Json
-import com.badlogic.gdx.utils.SerializationException
 import ktx.collections.GdxArray
 import ktx.collections.GdxMap
 import ktx.collections.set
@@ -14,13 +13,12 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 
-private const val SAVE_FILES = "save1.dat,save2.dat,save3.dat,save4.dat,save5.dat"
-private const val LOADING = ",Loading...,,,"
+private const val SAVE_FILES = "save1.dat,save2.dat,save3.dat,save4.dat,save5.dat,save6.dat,save7.dat"
+private const val LOADING = ",,Loading...,,,,"
 private const val SAVE_STATE_KEY = "saveState"
 private const val PROFILE_ID = "id"
 private const val PROFILE_SAVE_DATE = "saveDate"
 private const val DATE_PATTERN = "yyyy-MM-dd HH:mm"
-private const val INVALID_PROFILE_VIEW = " [Invalid]"
 private const val DEFAULT_EMPTY_PROFILE_VIEW = " [...]"
 
 class ProfileManager {
@@ -38,9 +36,8 @@ class ProfileManager {
     fun createNewProfile(profileId: String) {
         currentSaveFileName = getSaveFileNames()[selectedIndex]
         saveStateProperties.clear()
-        setProperty(PROFILE_ID, profileId.ifEmpty { getDefaultId() })
         brokerManager.profileObservers.notifyCreateProfile(this)
-        writeProfileToDisk()
+        writeProfileToDisk(profileId.ifEmpty { getDefaultId() })
     }
 
     @Suppress("NON_PUBLIC_CALL_FROM_PUBLIC_INLINE")
@@ -54,7 +51,7 @@ class ProfileManager {
 
     fun saveProfile() {
         brokerManager.profileObservers.notifySaveProfile(this)
-        writeProfileToDisk()
+        writeProfileToDisk(null)
     }
 
     fun loadProfile(profileIndex: Int) {
@@ -80,11 +77,12 @@ class ProfileManager {
         return saveStateProperties["mapTitle"] as String
     }
 
-    private fun writeProfileToDisk() {
-        val formatter = DateTimeFormatter.ofPattern(DATE_PATTERN)
-        setProperty(PROFILE_SAVE_DATE, formatter.format(LocalDateTime.now()))
+    private fun writeProfileToDisk(profileId: String?) {
+        val saveDate: String = DateTimeFormatter.ofPattern(DATE_PATTERN).format(LocalDateTime.now())
         val saveStateJsonString = json.prettyPrint(json.toJson(saveStateProperties))
         val saveFile = Gdx.app.getPreferences(currentSaveFileName)
+        if (profileId != null) saveFile.putString(PROFILE_ID, profileId)
+        saveFile.putString(PROFILE_SAVE_DATE, saveDate)
         saveFile.putString(SAVE_STATE_KEY, saveStateJsonString)
         saveFile.flush()
     }
@@ -92,12 +90,7 @@ class ProfileManager {
     private fun getVisualOf(profileIndex: Int): String {
         val saveFile = getSaveFileBy(profileIndex)
         return if (saveFile.contains(SAVE_STATE_KEY)) {
-            try {
-                val saveState = getSaveStateProperties(saveFile)
-                "${saveState[PROFILE_ID]} [${saveState[PROFILE_SAVE_DATE]}]"
-            } catch (e: SerializationException) {
-                "${profileIndex + 1} $INVALID_PROFILE_VIEW"
-            }
+            "${saveFile.getString(PROFILE_ID)} [${saveFile.getString(PROFILE_SAVE_DATE)}]"
         } else {
             "${profileIndex + 1} $DEFAULT_EMPTY_PROFILE_VIEW"
         }
@@ -114,7 +107,7 @@ class ProfileManager {
     }
 
     private fun getDefaultId(): String {
-        return "${Constant.PLAYER_ID.substring(0, 1).uppercase()} ${Constant.PLAYER_ID.substring(1)}"
+        return Constant.PLAYER_ID.replaceFirstChar { it.uppercase() }
     }
 
     private fun getSaveFileNames(): List<String> = SAVE_FILES.split(",")
